2a446abffec52959078a2c91eb9ecc0a51018522
ovaphlow <ovaphlow@live.com>
2021-04-03 16:26:19 +0800

    staff

--- route-miscellaneous.js
+++ route-miscellaneous.js
@@ -58,3 +58,87 @@ router.put('/feedback/:id', async (ctx) => {
     ctx.response.status = 500;
   }
 });
+
+router.get('/staff/filter', async (ctx) => {
+  try {
+    const sql = `
+        select id, uuid, username, name
+        from mis_user
+        order by id desc
+        limit 100
+        `;
+    const pool = mysql.promise();
+    const [result] = await pool.query(sql);
+    ctx.response.body = result;
+  } catch (err) {
+    logger.error(err.stack);
+    ctx.response.status = 500;
+  }
+});
+
+router.get('/staff/:id', async (ctx) => {
+  try {
+    const sql = 'select id, uuid, username, name from mis_user where id = ? and uuid = ? limit 1';
+    const pool = mysql.promise();
+    const [result] = await pool.query(sql, [parseInt(ctx.params.id), ctx.request.query.uuid]);
+    ctx.response.body = result.length === 1 ? result[0] : {};
+  } catch (err) {
+    logger.error(err.stack);
+    ctx.response.status = 500;
+  }
+});
+
+router.put('/staff/:id', async (ctx) => {
+  try {
+    const sql = `
+        update mis_user
+        set username = ?
+          , name = ?
+        where id = ?
+          and uuid = ?
+        `;
+    const pool = mysql.promise();
+    await pool.execute(sql, [
+      ctx.request.body.username,
+      ctx.request.body.name,
+      parseInt(ctx.params.id),
+      ctx.request.query.uuid,
+    ]);
+    ctx.response.status = 200;
+  } catch (err) {
+    logger.error(err.stack);
+    ctx.response.status = 500;
+  }
+});
+
+router.delete('/staff/:id', async (ctx) => {
+  try {
+    const sql = 'delete from mis_user where id = ? and uuid = ?';
+    const pool = mysql.promise();
+    await pool.execute(sql, [parseInt(ctx.params.id), ctx.request.query.uuid]);
+    ctx.response.status = 200;
+  } catch (err) {
+    logger.error(err.stack);
+    ctx.response.status = 500;
+  }
+});
+
+router.post('/staff', async (ctx) => {
+  try {
+    const sql = `
+        insert into
+          mis_user (uuid, username, password, name)
+          values (uuid(), ?, ?, ?)
+        `;
+    const pool = mysql.promise();
+    await pool.execute(sql, [
+      ctx.request.body.username,
+      ctx.request.body.password,
+      ctx.request.body.name,
+    ]);
+    ctx.response.status = 200;
+  } catch (err) {
+    logger.error(err.stack);
+    ctx.response.status = 500;
+  }
+});
